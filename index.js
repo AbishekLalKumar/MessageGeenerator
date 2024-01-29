2333b724bc8763de8ef383a8bfb6abac219baa5d
RakanXza <abishek.lalkumar@gmail.com>
2024-01-29 10:13:29 +0530

    Complete Project commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const messageOpening = ['Embrace', 'Seize', 'Radiate', 'Ignite', 'Lightup']
+const messageMid = ['overcome obstacles', 'face challenges', 'achieve your dreams', 'inspire greatness', 'cultivate positivity']
+const messageEnd = ['Never give up', 'You are capable', 'Believe in yourself', 'Brighter path awaits', 'Your future shines']
+const messageRef = ['Fellow', 'Friend', 'Buddy', 'Cheers', 'Legend' ]
+
+function messageGenerator () {
+
+    let a = Math.floor(Math.random() * 4) + 0;
+    let b = Math.floor(Math.random() * 4) + 0;
+    let c = Math.floor(Math.random() * 4) + 0;
+    let d = Math.floor(Math.random() * 4) + 0;
+
+    console.log('Hello, its working');
+    document.getElementById('message').innerHTML = `"${messageOpening[a]}, ${messageMid[b]}, ${messageEnd[c]}, ${messageRef[d]}"`;
+    console.log(`${messageOpening[a]}, ${messageMid[b]}, ${messageEnd[c]}, ${messageRef[d]}`);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is inspirational message generator program. 
+
+The project will have a main screen and a button. each time the user clicks the button the will be a new message displayed replacing the previouse message.
